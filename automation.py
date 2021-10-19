6fa44d4dcbf96f3a737ff5a5a7dbc1f2457ab5dc
rumsik <39966511+rumsik@users.noreply.github.com>
2021-10-19 20:55:56 +0300

    Create automation.py

---
+++ automation.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+driver = webdriver.Chrome()
+
+driver.get('https://www.youtube.com/')
+searchbox = driver.find_element_by_xpath('//*[@id="search"]')
+searchbox.send_keys('adv-it')
+
+searchbutton = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]/yt-icon')
+searchbutton.click()
